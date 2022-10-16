5f27a991b2c894be7813f95dc00097100ae6e203
Saqwix <iva2012.lol@gmail.com>
2022-10-16 13:06:16 +0300

    Задача1ДЗ

--- Task00/Program.cs
+++ Task00/Program.cs
@@ -7,7 +7,26 @@
 // -3 -> 9
 // -7 -> 49
 
-int num = Convert.ToInt32(Console.ReadLine());
-int square = num * num;
-Console.WriteLine(square);
-Console.WriteLine($"Квадрат числа {num} - {square}");
+// int num = Convert.ToInt32(Console.ReadLine());
+// int square = num * num;
+// Console.WriteLine(square);
+// Console.WriteLine($"Квадрат числа {num} - {square}");
+
+
+// if (num > 5 && num < 10)
+// {
+
+// }
+
+
+// Задача_1
+// a = 5; b = 7 -> max = 7
+// a = 2; b = 10 -> max = 10
+// a = -9; b = -3 -> max = -3
+
+
+Console.WriteLine("Введите первое число:");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите второе число:");
+int b = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine(a > b ? "Первое число больше второго" : "Второе число больше первого");
