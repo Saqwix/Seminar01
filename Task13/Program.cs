39ece32302f0fc0d86adacdd934ee61f40bd97ce
Saqwix <iva2012.lol@gmail.com>
2022-10-24 00:01:55 +0300

    task13

--- Task13/Program.cs
+++ Task13/Program.cs
@@ -2,3 +2,16 @@
 // 1. Напишите программу, которая выводит третью цифру заданного числа
 // 2. или сообщает, что третьей цифры нет. 
 
+Console.Write("Введите любое число: ");
+int num = Convert.ToInt32(Console.ReadLine()); 
+int count = (int)Math.Log10(num)-2;
+if (count < 0)
+{
+    Console.WriteLine("третьей цифры нет");
+}
+else
+    {
+        Console.WriteLine((num % (int)
+        Math.Pow(10, count + 1) / (int)Math.Pow(10, count)).ToString());
+        Console.ReadLine();
+    }
