80dc7140b1453ff05b93a96668b1db851a22b01a
Saqwix <iva2012.lol@gmail.com>
2022-10-23 20:33:44 +0300

    Task15

--- Task15/Program.cs
+++ Task15/Program.cs
@@ -9,11 +9,14 @@
 Console.Write("Введите число: ");
 int a = Convert.ToInt32(Console.ReadLine());
 
-if (a==1) Console.WriteLine("Рабочий");
-if (a==2) Console.WriteLine("Рабочий");
-if (a==3) Console.WriteLine("Рабочий");
-if (a==4) Console.WriteLine("Рабочий");
-if (a==5) Console.WriteLine("Рабочий");
-if (a==6) Console.WriteLine("Выходной");
-if (a==7) Console.WriteLine("Выходной");
-if (a > 7 || a < 1 ) Console.Write ("Такого дня недели не существует");
+if (a > 0 && a < 8 )
+{
+    if (a==1) Console.WriteLine("Рабочий");
+    else if (a==2) Console.WriteLine("Рабочий");
+    else if (a==3) Console.WriteLine("Рабочий");
+    else if (a==4) Console.WriteLine("Рабочий");
+    else if (a==5) Console.WriteLine("Рабочий");
+    else if (a==6) Console.WriteLine("Выходной");
+    else Console.WriteLine("Выходной");
+}
+else Console.Write ("Такого дня недели не существует");
