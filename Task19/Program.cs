6862352d2363c669ee22fcac4765a6ea3595620f
Saqwix <iva2012.lol@gmail.com>
2022-10-26 13:23:26 +0300

    task19 compl

--- Task19/Program.cs
+++ Task19/Program.cs
@@ -5,3 +5,28 @@
 // 14212 -> нет
 // 12821 -> да
 // 23432 -> да
+
+
+    Console.WriteLine("Введите число: ");
+    int number = Convert.ToInt32(Console.ReadLine());
+    int temp = number;
+    int number1 = 0;
+ if(number >= 10000 && number <= 99999)
+ {
+    while (temp != 0)
+    {
+          number1 = number1 * 10 + temp % 10;
+          temp /= 10;
+    }
+ 
+    if (number == number1)
+          Console.WriteLine("${number} - Да");
+    else
+          Console.WriteLine("${number} - Нет");
+    
+    
+}
+else
+{
+    Console.WriteLine($"ОШИБКА: {number} - Не является пятизначным");
+}
