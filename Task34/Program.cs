b2f4fa71191820b7b2b18ac78b70bba8931a5565
Saqwix <iva2012.lol@gmail.com>
2022-11-16 14:58:56 +0300

    competed task34

--- Task34/Program.cs
+++ Task34/Program.cs
@@ -1,2 +1,38 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 34: 
+// 1. Задайте массив заполненный случайными положительными трёхзначными числами. 
+// 2. Напишите программу, которая покажет количество чётных чисел в массиве.
+// [345, 897, 568, 234] -> 2
+
+Console.WriteLine("Введите размер массива");
+int size = Convert.ToInt32(Console.ReadLine());
+int[] arr = new int[size];
+
+FillArrayRandomarrbers(arr);
+Console.WriteLine("array:");
+
+PrintArray(arr);
+int count = 0;
+for (int m = 0; m < arr.Length; m++)
+if (arr[m] % 2 == 0)
+count++;
+Console.WriteLine($" {arr.Length} чисел в масиве | {count} четных ");
+
+
+void FillArrayRandomarrbers(int[] arr)
+{
+    for(int i = 0; i < arr.Length; i++)
+    {
+        arr[i] = new Random().Next(100,999);
+    }
+}
+void PrintArray(int[] arr)
+{
+
+    Console.Write("[");
+    for (int i = 0; i < arr.Length; i++)
+    {
+        if (i!=arr.Length -1)Console.Write($" {arr[i]} ");
+        else Console.Write($"{arr[i]}");
+    }
+    Console.WriteLine("]");
+}
