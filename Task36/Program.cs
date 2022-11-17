35e47d7d2cfaed7bfa9e6fe2042a1dd5a0c68cc3
Saqwix <iva2012.lol@gmail.com>
2022-11-17 13:10:41 +0300

    completed task36

--- Task36/Program.cs
+++ Task36/Program.cs
@@ -1,2 +1,37 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 36: 
+// 1. Задайте одномерный массив, заполненный случайными числами. 
+// 2. Найдите сумму элементов, стоящих на нечётных позициях.
+// [3, 7, 23, 12] -> 19
+// [-4, -6, 89, 6] -> 0
+
+Console.WriteLine("Введите размер массива");
+int size = Convert.ToInt32(Console.ReadLine());
+int[] arr = new int[size];
+FillArrayRandomarrbers(arr);
+Console.WriteLine("Масив: ");
+PrintArray(arr);
+int sum = 0;
+
+for( int d = 0; d < arr.Length; d+=2)
+sum = sum + arr[d];
+Console.WriteLine($"в масиве всего {arr.Length} чисел |  Cумма нечетных чисел, {sum}");
+
+void FillArrayRandomarrbers(int[] arr)
+{
+    for(int i = 0; i < arr.Length; i++)
+    {
+        arr[i] = new Random().Next(1,10);
+    }
+}
+
+void PrintArray(int[] arr)
+{
+
+    Console.Write("[");
+    for (int i = 0; i < arr.Length; i++)
+    {
+        if (i!=arr.Length -1)Console.Write($" {arr[i]} ");
+        else Console.Write($"{arr[i]}");
+    }
+    Console.WriteLine(" ]");
+}
