4d5211625c9f5c5f0194da608056ac877a6dacf2
Saqwix <iva2012.lol@gmail.com>
2022-12-01 11:47:40 +0300

    task66 completed

---
+++ Task66/Program.cs
@@ -0,0 +1,33 @@
+﻿// Задача 66: 
+// 1. Задайте значения M и N. Напишите программу, которая найдёт сумму натуральных элементов в промежутке от M до N. 
+// 2. Выполнить с помощью рекурсии.
+
+// M = 1; N = 15 -> 120
+// M = 4; N = 8. -> 30
+
+Console.Write("Введите число M: ");
+int m = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Введите число N: ");
+int n = Convert.ToInt32(Console.ReadLine());
+
+int temp = m;
+
+if (m > n) 
+{
+  m = n; 
+  n = temp;
+}
+
+PrintSum(m, n, temp=0);
+
+void PrintSum(int m, int n, int sum)
+{
+  sum = sum + n;
+  if (n <= m)
+  {
+    Console.Write($"Сумма элементов= {sum} ");
+    return;
+  }
+  PrintSum(m, n - 1, sum);
+}
